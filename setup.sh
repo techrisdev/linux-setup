e977b19986e9c32b1fe71d1fc180de43dc8bc8b5
techrisdev <techrisdev>
2021-11-14 18:05:51 +0100

    Rustup instead of installing cargo through the Package Manager

--- setup.sh
+++ setup.sh
@@ -53,7 +53,8 @@ $INSTALL_COMMAND exa
 # TODO: Window Manager, Polybar etc.
 
 # Alacritty Terminal
-$INSTALL_COMMAND cargo
+# Rustup
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 $INSTALL_COMMAND cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3
 cargo install alacritty
 
