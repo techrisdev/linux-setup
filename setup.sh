c3b6aedc63ad914c836c58090367218259f437fd
techrisdev <techrisdev>
2021-11-14 16:38:03 +0100

    Initial Commit

---
+++ setup.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+PACKAGE_MANAGER=apt
+INSTALL_COMMAND="sudo $PACKAGE_MANAGER install"
+
+echo "Assuming your Package Manager is $PACKAGE_MANAGER"
+echo "Assuming you're a normal user and sudo is installed and configured"
+
+echo "-- UPGRADING THE SYSTEM --"
+sudo apt update && sudo apt upgrade
+
+echo "-- DOTFILES --"
+$INSTALL_COMMAND git
+$INSTALL_COMMAND stow
+git clone --depth=1 https://github.com/techrisdev/dotfiles.git ~/.dotfiles
+cd ~/.dotfiles
+./Create_Symlinks_Linux.sh
+cd ~
+
+echo "-- INSTALLING PACKAGES --"
+# neovim as a editor
+$INSTALL_COMMAND neovim
+# Xorg as Display Server
+$INSTALL_COMMAND xorg
+# zsh as a shell
+$INSTALL_COMMAND zsh
+$INSTALL_COMMAND zsh-autocompletion
+$INSTALL_COMMAND zsh-autosuggestions
+$INSTALL_COMMAND zsh-syntax-highlighting
+# Starship as a shell prompt
+$INSTALL_COMMAND starship
+
+$INSTALL_COMMAND exa
+
+# TODO: Window Manager, Polybar etc.
+
+echo "-- ZSH --"
+# Set the default shell to zsh
+echo "Changing the default shell. Your password will be required."
+chsh -s /usr/bin/zsh
